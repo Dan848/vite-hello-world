8d00b01ec72c0ca8eb7358973a876127ea164df2
Dan848 <daniele.ceravolo15@gmail.com>
2023-04-12 17:23:25 +0200

    add hero section

---
+++ src/components/AppHero.vue
@@ -0,0 +1,33 @@
+:<template>
+    <div class="container-fluid bg-warning py-5 rounded-5">
+        <div class="container">
+            <div class="row gx-5">
+                <div class="col-12 col-md-6 d-flex align-items-center juststify-content-center mb-3">
+                    <div class="bg-white p-4 rounded-5 text-center">
+                    <h2 class="mb-3">Titolo Hero</h2>
+                    <div class="fs-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed reprehenderit beatae blanditiis! Non, eligendi dolores architecto labore atque quia quaerat dolor corporis inventore quisquam mollitia saepe aspernatur a porro aliquid quasi reiciendis impedit facilis qui modi praesentium unde? Molestias, error.</div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 mb-3">
+                    <img class="img-fluid" src="../assets/img/hero.png" alt="monitor">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppHero",
+        data() {
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,5 +1,5 @@
 :<template>
-    <header class="container text-center mt-5">
+    <header class="container text-center my-5">
         <h1>{{title}}</h1>
     </header>
 </template>
