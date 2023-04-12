42159977ca241cdfd37738167b7d61bd3e01189c
Dan848 <daniele.ceravolo15@gmail.com>
2023-04-12 15:56:03 +0200

    Create title

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppTitle />
+  
+</template>
+
+<script>
+  import AppTitle from './components/AppTitle.vue';
+  export default {
+    components: {
+      AppTitle
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+:<template>
+    <header class="container text-center mt-5">
+        <h1>{{title}}</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "AppTitle",
+        data() {
+            return{
+                title: "La mia prima App con Vite!"
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
